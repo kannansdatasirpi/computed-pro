a2d6d5f8f8e3e9e55c7d7cf1112d02a706fbe92c
kannan <kannans@datasirpi.com>
2022-11-22 15:10:39 +0530

    new-commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <new-component>
+  </new-component>
+</template>
+
+<script>
+import NewComponent from './components/NewComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NewComponent,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #165ea6;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/NewComponent.vue
@@ -0,0 +1,33 @@
+<!--In Vue.js, computed properties enable you to create a property that can be used to modify, manipulate, 
+    and display data within your components in a readable and efficient manner.-->
+<template>
+        <p>Has Published Book:</p>
+        <span>{{author.books.length > 0 ? 'Yes' : 'No'}}</span>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      author: {
+        name: 'John Doe',
+        books: [
+          'Vue 2 - Advanced Guide',
+          'Vue 3 - Basic Guide',
+          'Vue 4 - The Mystery'
+        ]
+      }
+    }
+  },
+  computed: {
+    publishedBooksMessage() {
+      return this.author.books.length > 0 ? 'Yes' : 'No'
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
